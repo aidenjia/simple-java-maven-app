eebd0ffe91e90b19a572d98d8caf17718e8b5b21
Aiden_Jia <Aiden_Jia@epam.com>
2022-11-17 17:03:44 +0800

    Commit4 m

--- src/main/java/com/mycompany/app/Commit1.java
+++ src/main/java/com/mycompany/app/Commit1.java
@@ -2,6 +2,8 @@ package com.mycompany.app;
 
 public class Commit1 {
     public static void main(String[] args) {
+        System.out.println("1231");
+        System.out.println("12314");
       System.out.println("V");
     }
 }
