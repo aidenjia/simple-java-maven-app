8bbe359369493c9aadbef5c29f7b4278fab3cdb9
Aiden_Jia <Aiden_Jia@epam.com>
2022-11-17 17:25:24 +0800

    dev1

--- src/main/java/com/mycompany/app/Commit1V.java
+++ src/main/java/com/mycompany/app/Commit1V.java
@@ -1,7 +1,9 @@
 package com.mycompany.app;
 
 public class Commit1V {
+
     public static void main(String[] args) {
+        System.out.println("dev1");
         System.out.println("master2");
         System.out.println("master3");
     }
